4357062d73973f41e07d27bef761d642e53b3d10
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-04-05 15:06:11 -0400

    commit tree 26d5b23b5577092f051d991dcadca93b67b134ab

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for(let j =i + 1; j < array.length; j++){
+      let sum = array[i] + array[j]
+      if (sum === target) {return true};
+      }
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+The function will take and array and a target value
+  It will compare the sum of any two numbers in the array against the target valuee
+    if they match, return true
 */
 
 /*
-  Add written explanation of your solution here
+  As we are needing to run through the array and need to compare every value against the others, we need to create a loop within a loop with the first loop starting the iteration through the array and grabbing one element and the second loop grabbing another element in the array. and so on
 */
 
 // You can run `node index.js` to view these console logs
